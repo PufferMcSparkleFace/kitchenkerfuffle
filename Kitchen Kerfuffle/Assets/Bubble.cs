1ea17408ca57c3742ee743958174d6fb07703f83
Daniel Gutierrez <83122636+PufferMcSparkleFace@users.noreply.github.com>
2023-10-17 17:42:31 +1300

    Bouncing works, but direction is weird

--- Kitchen Kerfuffle/Assets/Bubble.cs
+++ Kitchen Kerfuffle/Assets/Bubble.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class Bubble : MonoBehaviour
 {
-
+    Vector3 lastVelocity;
     public Rigidbody2D bubbleRB;
+    public float bubbleSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        bubbleRB = GetComponent<Rigidbody2D>();
+        bubbleRB.AddForce(new Vector2(bubbleSpeed, bubbleSpeed));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        lastVelocity = bubbleRB.velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var speed = lastVelocity.magnitude;
+        var direction = Vector3.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
+
+        bubbleRB.velocity = direction * Mathf.Max(speed, 0f);
     }
 }
