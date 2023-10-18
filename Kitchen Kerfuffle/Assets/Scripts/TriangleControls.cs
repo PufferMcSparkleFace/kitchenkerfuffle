decb5938c966c0cd0a7109e56b4aeb14ccf87160
Daniel Gutierrez <83122636+PufferMcSparkleFace@users.noreply.github.com>
2023-10-19 01:21:54 +1300

    removing fixed update

--- Kitchen Kerfuffle/Assets/Scripts/TriangleControls.cs
+++ Kitchen Kerfuffle/Assets/Scripts/TriangleControls.cs
@@ -87,12 +87,8 @@ public class TriangleControls : MonoBehaviour
         {
             aimIndicator.enabled = false;
         }
-    }
 
-    private void FixedUpdate()
-    {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-
     }
 
     private bool IsGrounded()
