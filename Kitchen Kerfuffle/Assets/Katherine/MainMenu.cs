2c8929bee1d48b0ee199e01173b21861597a6ebf
AeroSpecsDev <145954433+AeroSpecsDev@users.noreply.github.com>
2023-10-25 22:40:19 +1300

    slight fixing in code and scene

--- Kitchen Kerfuffle/Assets/Katherine/MainMenu.cs
+++ Kitchen Kerfuffle/Assets/Katherine/MainMenu.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void LoadScene()
+    public void LoadCharacterScene()
     {
         SceneManager.LoadScene("CharacterSelection");
     }
