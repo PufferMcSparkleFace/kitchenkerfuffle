13050e202534a51f585c55089afc4bf1cc50f4ea
AeroSpecsDev <145954433+AeroSpecsDev@users.noreply.github.com>
2023-10-25 18:43:18 +1300

    menu music get carried through
    problem is it also carried through to gameplay scene

--- Kitchen Kerfuffle/Assets/Katherine/AudioManager.cs
+++ Kitchen Kerfuffle/Assets/Katherine/AudioManager.cs
@@ -1,14 +1,31 @@
 ﻿using UnityEngine.Audio;
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
 
     public Sound[] sounds;
 
-    void Awake()
+    public static AudioManager instance;
+
+    void Awake() //initialize the class method is in
     {
+        //for music to continue to play in the next scene
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+
+
+        //changing music pitch n volume
         foreach (Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
@@ -22,13 +39,38 @@ public class AudioManager : MonoBehaviour
 
     private void Start()
     {
+
         Play("MenuMusic");
-        Play("CharacterSelectionMusic");
+        Play("BattleBGMusic");
     }
 
-    public void Play (string name)
+    public void Play(string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + "not found!");
+            return;
+        }
         s.source.Play();
+
     }
+
+    public void StopPlaying(string sound)
+    {
+        Sound s = Array.Find(sounds, item => item.name == sound);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+
+        s.source.volume = s.volume * (1f + UnityEngine.Random.Range(-s.volume / 2f, s.volume / 2f));
+        s.source.pitch = s.pitch * (1f + UnityEngine.Random.Range(-s.pitch / 2f, s.pitch / 2f));
+
+        s.source.Stop();
+    }
+
+   
 }
+
