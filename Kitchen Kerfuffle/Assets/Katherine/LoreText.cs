c322c051ae3ca983268dcac2b52c4846c4cbb383
AeroSpecsDev <145954433+AeroSpecsDev@users.noreply.github.com>
2023-10-26 22:27:20 +1300

    character's lore
    added character description in character selection scene

---
+++ Kitchen Kerfuffle/Assets/Katherine/LoreText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoreText : MonoBehaviour
+{
+    public GameObject logoText;
+
+    void Start()
+    {
+        logoText.SetActive(false);
+    }
+
+    public void OnMouseOver()
+    {
+        logoText.SetActive(true);
+    }
+
+    public void OnMouseExit()
+    {
+        logoText.SetActive(false);
+    }
+}
