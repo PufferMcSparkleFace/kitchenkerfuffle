ce8376f5d66eb6476917f452dcca0384885f7c60
AeroSpecsDev <145954433+AeroSpecsDev@users.noreply.github.com>
2023-10-27 19:41:40 +1300

    heart

--- Kitchen Kerfuffle/Assets/Scripts/ScoreTracker.cs
+++ Kitchen Kerfuffle/Assets/Scripts/ScoreTracker.cs
@@ -10,6 +10,9 @@ public class ScoreTracker : MonoBehaviour
 
     public int triangleLives = 3;
     public int circleLives = 3;
+    public Image[] triangleHearts;
+    public Image[] circleHearts;
+    public GameObject[] fullHeart;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +23,32 @@ public class ScoreTracker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (triangleLives < 1)
+        {
+            Destroy(fullHeart[0].gameObject);
+        }
+        else if (triangleLives < 2)
+        {
+            Destroy(fullHeart[1].gameObject);
+        }
+        else if (triangleLives < 3)
+        {
+            Destroy(fullHeart[2].gameObject);
+        }
+
+        if (circleLives < 1)
+        {
+            Destroy(fullHeart[0].gameObject);
+        }
+        else if (circleLives < 2)
+        {
+            Destroy(fullHeart[1].gameObject);
+        }
+        else if (circleLives < 3)
+        {
+            Destroy(fullHeart[2].gameObject);
+        }
+
         if (Input.GetKeyDown("space"))
         {
             TriangleHit();
@@ -33,20 +62,24 @@ public class ScoreTracker : MonoBehaviour
         if(circleLives == 0)
         {
             // death animation for circle
-
             Debug.Log("Circle Loses!"); 
         }
     }
 
+    // Triangle Damage
     public void TriangleHit()
     {
         triangleLives--;
         Debug.Log("Triangle Hit!");
+
     }
 
+    // Circle Damange
     public void CircleHit()
     {
         circleLives--;
         Debug.Log("Circle Hit!");
+
     }
+
 }
